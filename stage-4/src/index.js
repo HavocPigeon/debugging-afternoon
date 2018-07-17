6d89313156d0edd36ae64f39ea700e554c5f4676
HavocPigeon <speeas@gmail.com>
2018-07-17 14:48:22 -0700

    fixed stage4 bugs

--- stage-4/src/index.js
+++ stage-4/src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
 
-    <Provider store={store}>
-        <App />
+    <Provider store={store}><BrowserRouter>
+        <App /></BrowserRouter>
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
